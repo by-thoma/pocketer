a318f778c50354b750f31fda4ae7b55ec24f3e69
thoma <toma0805@yandex.ru>
2022-08-27 17:34:36 +0300

    added yaml files

--- pkg/telegram/error.go
+++ pkg/telegram/error.go
@@ -1,1 +1,35 @@
 package telegram
+
+import (
+	"errors"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+var (
+	errInvalidURL   = errors.New("url is invalid")
+	errUnauthorized = errors.New("user is not authorized")
+	errUnableToSave = errors.New("unable to save")
+)
+
+//msg.Text = "Ты не авторизирован"
+//msg.Text = "Это невалидная ссылка"
+//msg.Text = "Не удалость сохранить ссылку"
+func (b *Bot) handleError(chatID int64, err error) {
+	msg := tgbotapi.NewMessage(chatID, "Произошла неизвестная ошибка")
+
+	switch err {
+	case errInvalidURL:
+		msg.Text = "Это невалидная ссылка"
+		b.bot.Send(msg)
+	case errUnauthorized:
+		msg.Text = "Ты не авторизирован"
+		b.bot.Send(msg)
+	case errUnableToSave:
+		msg.Text = "Не удалость сохранить ссылку"
+		b.bot.Send(msg)
+	default:
+		b.bot.Send(msg)
+
+	}
+
+}
--- pkg/telegram/handlers.go
+++ pkg/telegram/handlers.go
@@ -1,7 +1,9 @@
 package telegram
 
 import (
-	"log"
+	"context"
+	"github.com/zhashkevych/go-pocket-sdk"
+	"net/url"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
@@ -23,11 +25,27 @@ func (b *Bot) handleCommand(message *tgbotapi.Message) error {
 	}
 }
 
-func (b *Bot) handleMessage(message *tgbotapi.Message) {
-	log.Printf("[%s] %s", message.From.UserName, message.Text)
+func (b *Bot) handleMessage(message *tgbotapi.Message) error {
+	_, err := url.ParseRequestURI(message.Text)
+	if err != nil {
+		return errInvalidURL
+	}
 
-	msg := tgbotapi.NewMessage(message.Chat.ID, message.Text)
-	b.bot.Send(msg)
+	accessToken, err := b.getAccessToken(message.Chat.ID)
+	if err != nil {
+		return errUnauthorized
+	}
+
+	if err := b.pocketClient.Add(context.Background(), pocket.AddInput{
+		AccessToken: accessToken,
+		URL:         message.Text,
+	}); err != nil {
+		return errUnableToSave
+	}
+
+	msg := tgbotapi.NewMessage(message.Chat.ID, "Ссылка сохранена")
+	_, err = b.bot.Send(msg)
+	return err
 }
 
 func (b *Bot) handleStartCommand(message *tgbotapi.Message) error {
